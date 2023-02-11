a190edc81cda59d5a53a2c336fa9448370bbf6f5
Mark Savage <122678639+0xMRKSVG@users.noreply.github.com>
2023-02-11 12:13:48 -0700

    content added

---
+++ scripts.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  document.getElementById("hello").innerHTML = "<p>“Hello, friend?”<br>That's lame.<br>Maybe I should give you a name.<br>But that's a slippery slope.<br>You’re only in my head. We have to remember that…<br>Shit.<br>It’s actually happened.<br>I’m talking to an imaginary person.</p><p>— Mr. Robot S01E01</p>";
+}
